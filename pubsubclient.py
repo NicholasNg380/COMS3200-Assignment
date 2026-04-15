a7b238ee2b490266276d5c15a363aa52ecaaf50d
Nicholas <NNYC380@outlook.com>
2026-04-15 16:09:54 +1000

    Fixed begining to be similar to previous assignment

--- pubsubclient.py
+++ pubsubclient.py
@@ -4,15 +4,11 @@ from sys import stdout,stdin,argv,exit
 
 BUFSIZE = 1024
 
-def handle_input(*arg):
-    len = len(arg)
-    if len == 0:
-        print("Usage: python pubsubclient.py <hostname> <port>")
-        exit(1)
+def main():
+    params = 
     
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    handle_input(*args)
+    main()
     
     
